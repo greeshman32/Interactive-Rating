aaa0527d627fbc6c9eba0fd5f6b0c7af9d16ed05
Greeshman <billagreeshman@gmail.com>
2022-07-27 22:01:03 +0530

    read me changes

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 let selected;
-let val;
+let val='0';
 
 const rate=document.getElementById("select");
 const submitButton=document.getElementById("submit");
 
 const changeStyle=(event)=>{
-    if(event.target.localName=='li'){
-            if(selected!==undefined){
+    if(event.target.localName=='li'){ //checks if the user has clicked on li or ul
+            if(selected!==undefined){ // change the previosly selected rating to normal
                 selected.style.backgroundColor=''
                 selected.style.color='';
             }
-            if(selected!=event.target){
+            if(selected!=event.target){ //checks if the event is already selected 
             event.target.style.backgroundColor='hsl(217, 12%, 63%)';
             event.target.style.color='white';
             selected=event.target;
             val=selected.innerText;
             }
-            else{
-                val=undefined;
+            else{ //this will unselect the event
+                val='0';
                 selected=undefined;
             }
         }
 };
 
 const submit=()=>{
-    if(val!=undefined){
             let ratingContainer=document.getElementsByClassName("rating");
             ratingContainer[0].style.transform='translateY(-200%)';
-            let thankyouContainer=document.getElementsByClassName("thankyou");
+
+            let thankyouContainer=document.getElementsByClassName("thankyou"); 
             thankyouContainer[0].style.transform='translateY(-105%)';
+
             let addrating=document.getElementById("add-rating");
             addrating.textContent=val;
-        }
 };
 
 
